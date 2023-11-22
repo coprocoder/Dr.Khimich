6e5714753b640558e6937f515021752d3ebbbd89
coprocoder <mog.ie@mail.ru>
2023-11-23 04:16:17 +0700

    add eslint, prettier

--- src/components/header/menu/lineMenu.js
+++ src/components/header/menu/lineMenu.js
@@ -1,41 +1,41 @@
-import React from "react";
-import { Link, Button } from "@mui/material";
-import { makeStyles } from "@mui/styles";
+import React from 'react'
+import { Link, Button } from '@mui/material'
+import { makeStyles } from '@mui/styles'
 
-import {menuConfig} from "./config";
+import { menuConfig } from './config'
+
+const HeaderMenuFull = () => {
+  return (<>
+    <MenuNav />
+    <Button href='#' variant='outlined' sx={{ my: 1, mx: 1.5 }}>
+      Связаться
+    </Button>
+  </>)
+}
+
+const MenuNav = () => {
+  const classes = useStyles()
+  return (<nav>
+    {Object.values(menuConfig).map((x, i) => (<Link
+      key={i}
+      variant='button'
+      // color="text.primary"
+      href={x.link}
+      sx={{ my: 1, mx: 1.5, color: 'var(--primary)' }}
+      className={classes.link}
+    >
+      {x.label}
+    </Link>))}
+  </nav>)
+}
 
 const useStyles = makeStyles({
   link: {
     // color: "var(--primary)",
-    "&:hover": {
-      color: "var(--primarySecond)",
+    '&:hover': {
+      color: 'var(--primarySecond)',
     },
   },
-});
+})
 
-const HeaderMenuFull = () => {
-  const classes = useStyles();
-  return (
-    <>
-      <nav>
-        {Object.values(menuConfig).map((x, i) => (
-          <Link
-            key={i}
-            variant="button"
-            // color="text.primary"
-            href={x.link}
-            sx={{my: 1, mx: 1.5, color: "var(--primary)"}}
-            className={classes.link}
-          >
-            {x.label}
-          </Link>
-        ))}
-      </nav>
-      <Button href="#" variant="outlined" sx={{my: 1, mx: 1.5}}>
-        Связаться
-      </Button>
-    </>
-  );
-};
-
-export default HeaderMenuFull;
+export default HeaderMenuFull
