9acc5ef54fd85852cfc3705f43b80e033b1a56e1
coprocoder <mog.ie@mail.ru>
2023-11-25 23:29:32 +0700

    separate router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import { Routes, Route, HashRouter } from 'react-router-dom'
-import loadable from '@loadable/component'
-import { Backdrop, CircularProgress } from '@mui/material'
-import Header from './components/header'
+import PageHeader from './components/header'
 import PageFooter from './components/footer'
 
 import { createTheme, ThemeProvider } from '@mui/material/styles'
 import { green, purple } from '@mui/material/colors'
+import Router from './router'
 
 const theme = createTheme({
   palette: {
@@ -15,42 +13,12 @@ const theme = createTheme({
   }
 })
 
-const Waiter = () => (
-  <Backdrop
-    sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-    open={true}
-  >
-    <CircularProgress color='inherit' />
-  </Backdrop>
-)
-
-const HomePage = loadable(() => import('./pages/home'), {
-  fallback: <Waiter />
-})
-const ProductsGallery = loadable(() => import('./pages/products'), {
-  fallback: <Waiter />
-})
-const PriceGallery = loadable(() => import('./pages/prices'), {
-  fallback: <Waiter />
-})
-
-const NotFound = () => <div>Sorry, page not found. Go back.</div>
-
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <HashRouter>
-        <Header />
-
-        <Routes>
-          <Route exact path='/' element={<HomePage />} />
-          <Route path='/products' element={<ProductsGallery />} />
-          <Route path='/prices' element={<PriceGallery />} />
-          <Route element={<NotFound />} />
-        </Routes>
-
-        <PageFooter />
-      </HashRouter>
+      <PageHeader />
+      <Router />
+      <PageFooter />
     </ThemeProvider>
   )
 }
---
+++ src/router.js
@@ -0,0 +1,38 @@
+import { Backdrop, CircularProgress } from '@mui/material'
+import loadable from '@loadable/component'
+import React from 'react'
+import { HashRouter, Route, Routes } from 'react-router-dom'
+
+
+const Waiter = () => (
+  <Backdrop open={true} sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+    <CircularProgress color='inherit' />
+  </Backdrop>
+)
+
+const HomePage = loadable(() => import('./pages/home'), {
+  fallback: <Waiter />
+})
+const ProductsGallery = loadable(() => import('./pages/products'), {
+  fallback: <Waiter />
+})
+const PriceGallery = loadable(() => import('./pages/prices'), {
+  fallback: <Waiter />
+})
+
+const NotFound = () => <div>Sorry, page not found. Go back.</div>
+
+const Router = () => {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route exact path='/' element={<HomePage />} />
+        <Route path='/products' element={<ProductsGallery />} />
+        <Route path='/prices' element={<PriceGallery />} />
+        <Route element={<NotFound />} />
+      </Routes>
+    </HashRouter>
+  )
+}
+
+export default Router
